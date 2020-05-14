d6dcb2fc020ebd554199bbc71af6d9111e9cd2e9
Alexande Juca <corextechnologies@gmail.com>
2020-05-14 10:50:33 +0100

    Added getWeatherToday endpoint

--- src/main/java/com/alexjuca/okta/oktademo/controllers/HomeController.java
+++ src/main/java/com/alexjuca/okta/oktademo/controllers/HomeController.java
@@ -1,4 +1,27 @@
 package com.alexjuca.okta.oktademo.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
 public class HomeController {
+
+    @GetMapping("/")
+    public WeatherToday getWeatherToday(Principal principal) {
+        return new WeatherToday("10 C", "22 C", principal.getName());
+    }
+
+    public static class WeatherToday {
+        private final String lowTemp;
+        private final String highTemp;
+        private final String username;
+
+        public WeatherToday(String lowTemp, String highTemp, String username) {
+            this.lowTemp = lowTemp;
+            this.highTemp = highTemp;
+            this.username = username;
+        }
+    }
 }
